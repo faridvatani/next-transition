4a2cbf11bfae09753659b5d81bc1f8bb80a85301
Farid Vatani <faridvatani@gmail.com>
2023-09-01 09:22:15 +0330

    chore: removed app dir in next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  trailingSlash: true,
-  basePath: "/next-transition",
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
