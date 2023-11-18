449962d120e07d882cc7b47d7eb750f1a7fb4d33
Farid Vatani <faridvatani@gmail.com>
2023-11-18 13:46:06 +0330

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  assetPrefix: '/',
+  basePath: '/faridvatani.github.io',
 };
 
 module.exports = nextConfig;
