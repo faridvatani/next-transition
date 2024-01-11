b2e85f7f514bd96d787acc704c3a86d362271ecb
Farid Vatani <faridvatani@gmail.com>
2024-01-11 13:56:36 +0330

    Add trailing slash configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  skipTrailingSlashRedirect: true,
+  trailingSlash: true,
   assetPrefix:
     process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/next-transition' : '',
 };
