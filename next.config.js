53767ab9a77d683b5dd575cb6ec766aed994b7ae
Farid Vatani <faridvatani@gmail.com>
2024-01-11 13:47:21 +0330

    chore: add env file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix:
+    process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/next-transition' : '',
 };
 
 module.exports = nextConfig;
