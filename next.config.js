fd48f33fbe6e45d86f4bf442a85f16f6585c59f0
Farid Vatani <faridvatani@gmail.com>
2024-01-12 18:05:23 +0330

    Remove trailing slash configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  skipTrailingSlashRedirect: true,
-  trailingSlash: true,
   basePath:
     process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? '/next-transition' : '',
 };
